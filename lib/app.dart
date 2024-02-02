61c0db431ec6e2f81675b5165541d07a26faac79
GitSuman0699 <aseztak.suman@gmail.com>
2024-02-02 12:02:39 +0530

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_project/auth/signup/signup_screen.dart';
+import 'package:firebase_project/utils/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.system,
+      theme: AAppTheme.lightTheme,
+      darkTheme: AAppTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      home: const SignUpScreen(),
+    );
+  }
+}
