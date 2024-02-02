ba0cbb6b51d907b382a541faae103bb870dcd948
BetaLearning07 <learnibeta@gmail.com>
2024-02-02 12:18:10 +0530

    suman

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_project/utils/constants/app_routes.dart';
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+=======
+>>>>>>> ba7a797 (suman)
 import 'app.dart';
 import 'firebase_options.dart';
 
