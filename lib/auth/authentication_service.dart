ea03d099859569a6526e153895afa1a2d0a74443
BetaLearning07 <learnibeta@gmail.com>
2024-02-06 10:53:32 +0530

    suman

--- lib/auth/authentication_service.dart
+++ lib/auth/authentication_service.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_project/utils/exceptions/firebase_auth_exceptions.dart';
-import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthenticationService {
   static AuthenticationService get instance => AuthenticationService();
@@ -38,8 +38,33 @@ class AuthenticationService {
     }
   }
 
+  Future<UserCredential> signInWithGoogle() async {
+    try {
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } on FirebaseAuthException catch (e) {
+      throw AppFirebaseAuthException(e.code);
+    } catch (e) {
+      throw e.toString(); // Return an error message
+    }
+  }
+
   // Sign out
   Future signOut() async {
+    await GoogleSignIn().signOut();
     await _auth.signOut();
   }
 
--- lib/auth/login/components/login_footer.dart
+++ lib/auth/login/components/login_footer.dart
@@ -1,10 +1,21 @@
+import 'package:firebase_project/auth/authentication_service.dart';
+import 'package:firebase_project/utils/device/device_utility.dart';
 import 'package:flutter/material.dart';
 
-class SignUpFooter extends StatelessWidget {
-  const SignUpFooter({super.key});
+class LoginFooter extends StatelessWidget {
+  const LoginFooter({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: AppDeviceUtils.getScreenWidth(context),
+      child: ElevatedButton.icon(
+        onPressed: () async {
+          await AuthenticationService.instance.signInWithGoogle();
+        },
+        icon: const Icon(Icons.g_mobiledata_rounded),
+        label: const Text("SignIn With Google"),
+      ),
+    );
   }
 }
