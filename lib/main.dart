61feacd1ded4b3a30fac8eb95bbd67f28b46eb34
BetaLearning07 <learnibeta@gmail.com>
2024-03-27 08:56:54 +0530

    testing battery level

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_project/firebase_options.dart';
 import 'package:firebase_project/utils/constants/app_routes.dart';
 import 'package:firebase_project/utils/local_storage/storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
 
@@ -11,5 +12,22 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Prefs.instance.init();
   AppRoutes.setSystemStyling();
+  getBatteryLevel();
   runApp(const ProviderScope(child: MyApp()));
+}
+
+// Define the channel name
+const String batteryLevelChannel = 'dev.myapp.battery';
+
+// Create a MethodChannel instance
+final MethodChannel _batteryChannel = MethodChannel(batteryLevelChannel);
+
+Future<int> getBatteryLevel() async {
+  try {
+    final result = await _batteryChannel.invokeMethod('getBatteryLevel');
+    return result;
+  } on PlatformException catch (e) {
+    // Handle platform exceptions (e.g., missing functionality)
+    return 0;
+  }
 }
